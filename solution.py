aed31645d8d1eb86c2119164dc46cee331f94d46
Robert Graham <rpgraham84@gmail.com>
2016-09-26 01:51:42 -0400

    Finalized solution to problem

---
+++ solution.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+
+
+class InvalidNodesException(Exception):
+    pass
+
+
+class Tree(object):
+    """ Tree/Node object. Very simple, takes a parent and a value.
+    """
+    def __init__(self, value=None, parent=None):
+        self.value = value
+        self.parent = parent
+
+
+class Solution(object):
+    def __init__(self, input_lines):
+        self.input_lines = input_lines        
+        self.build_tree_from_input()
+
+    def build_tree_from_input(self):
+        self.forest = {}
+        for line in self.input_lines:
+            nodes = line.split()
+            if len(nodes) == 2:
+                # Not a root node
+                node_name, parent_name = nodes
+            elif len(nodes) == 1:
+                # Node is a root node, no parent specified
+                if nodes:
+                    node_name, parent_name = nodes[0], None
+            else:
+                # Deal with blank/invalid lines in input file
+                continue
+
+            node = self.forest.get(node_name)
+            parent = self.forest.get(parent_name)
+            if parent:
+                self.forest[node_name] = Tree(parent=parent, value=node_name)
+            else:
+                self.forest[node_name] = Tree(parent=None, value=node_name)
+
+    def get_parents(self, tree, path=[]):
+        """ Recursive function to get all parents in order from a node.
+        """
+        if tree:
+            path.append(tree.value)
+            if tree.parent:
+                path.append(self.get_parents(tree.parent, path))
+
+        return path
+
+    def solve(self, first_node, second_node):
+        first_node = self.forest.get(first_node)
+        second_node = self.forest.get(second_node)
+
+        if not first_node or not second_node:
+            raise InvalidNodesException("Invalid nodes given.")
+        elif first_node == second_node:
+            raise InvalidNodesException("Nodes are the same.")
+
+        path1 = [x for x in self.get_parents(
+            first_node, []) if type(x) == str][1:]
+        path2 = [x for x in self.get_parents(
+            second_node, []) if type(x) == str][1:]
+        nca = self.nearest_common_ancestor(path1, path2)
+
+        if nca:
+            return nca
+        else:
+            return "No common ancestor"
+
+    @staticmethod
+    def nearest_common_ancestor(first_path, second_path):
+        """ This function begins iterating over the shortest path until it finds 
+            the first ancestor the two lists have in common. It will be the 
+            nearest because the paths are represented as nodes in order back to 
+            their root.
+        """
+        if len(first_path) < len(second_path):
+            for node in first_path:
+                if node in second_path:
+                    return node
+        else:
+            for node in second_path:
+                if node in first_path:
+                    return node
+
+
+if __name__ == "__main__":
+    try:
+        input_file, first_node, second_node = sys.argv[1:]
+    except:
+        print("Check parameters and try again.\n"
+              "Usage: python solution.py <input_file> <node 1> <node 2>")
+        sys.exit(1)
+
+    with open(input_file) as f:
+        input_lines = f.readlines()
+
+    solution = Solution(input_lines)
+    print(solution.solve(first_node, second_node))
+
